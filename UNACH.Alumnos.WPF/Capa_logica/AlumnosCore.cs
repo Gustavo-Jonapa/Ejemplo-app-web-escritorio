a8a0afd4027220f877f9d43bc39e5ace5ef7add5
Gustavo Jonapa <gustavo.jonapa24@unach.mx>
2026-03-19 17:05:55 -0600

    AlumnosCore Insertar y Editar

--- UNACH.Alumnos.WPF/Capa_logica/AlumnosCore.cs
+++ UNACH.Alumnos.WPF/Capa_logica/AlumnosCore.cs
@@ -13,12 +13,96 @@ namespace UNACH.Alumnos.WPF.Capa_logica
 		/// Método para obtener alumnos
 		/// </summary>
 		/// <returns>lista de alumnos</returns>
-		public List<Alumno>ObtenerAlumnos()
+		public List<Alumno> ObtenerAlumnos()
 		{
-			using (AlumnosModel context =new AlumnosModel())
+			using (AlumnosModel context = new AlumnosModel())
 			{
 				var listaAlumnos = context.Alumnos.ToList();
 				return listaAlumnos;
+			}
+		}
+		/// <summary>
+		/// Consulta de alumnos filtrados
+		/// </summary>
+		/// <param name="nombre">campo string</param>
+		/// <returns>Lista de alumnos</returns>
+		public List<Alumno> ObtenerAlumnosPorNombre(string nombre)
+		{
+			using (var context = new AlumnosModel())
+			{
+				return (from a in context.Alumnos
+						where a.Nombre.Contains(nombre)
+						select a)
+					   .ToList();
+			}
+		}
+		public bool InsertarAlumno(Alumno alumno)
+		{
+			try
+			{
+				using (var context = new AlumnosModel())
+				{
+					context.Alumnos.Add(alumno);
+					context.SaveChanges();
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+		}
+		public bool EliminarAlumno(int id)
+		{
+			try
+			{
+				using (var context = new AlumnosModel())
+				{
+					var alumno = (from a in context.Alumnos
+								  where a.id == id
+								  select a).FirstOrDefault();
+					if (alumno != null)
+					{
+						context.Alumnos.Remove(alumno);
+						context.SaveChanges();
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+		}
+		public bool EditarAlumno(Alumno alumno)
+		{
+			try
+			{
+				using (var context = new AlumnosModel())
+				{
+					var _alumno = (from a in context.Alumnos
+								  where a.id == alumno.id
+								  select a).FirstOrDefault();
+					if (_alumno != null)
+					{
+						_alumno.Nombre = alumno.Nombre;
+						_alumno.Telefono = alumno.Telefono;
+						context.SaveChanges();
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				return false;
 			}
 		}
 	}
